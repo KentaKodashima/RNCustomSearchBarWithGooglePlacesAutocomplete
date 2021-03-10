92108370751641b861f9ccbefc775af4d79517b3
Kenta Kodashima <kentakodashima@gmail.com>
2021-03-09 16:30:21 -0800

    Add a search bar with basic styling.

---
+++ components/SearchBarWithAutocomplete.tsx
@@ -0,0 +1,56 @@
+import React, { FunctionComponent } from 'react'
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  ViewStyle
+} from 'react-native'
+
+type SearchBarProps = {
+  value: string
+  style?: ViewStyle | ViewStyle[]
+  onChangeText: (text: string) => void
+}
+
+const SearchBarWithAutocomplete: FunctionComponent<SearchBarProps> = props => {
+  const {
+    value,
+    style,
+    onChangeText
+  } = props
+
+  const {
+    container,
+    inputStyle
+  } = styles
+  const passedStyles = Array.isArray(style) ? Object.assign({}, ...style) : style
+
+  return (
+    <View style={[container, { ...passedStyles }]}>
+      <TextInput
+        style={inputStyle}
+        placeholder='Search by address'
+        placeholderTextColor='gray'
+        value={value}
+        onChangeText={onChangeText}
+        returnKeyType='search'
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center'
+  },
+  inputStyle: {
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    backgroundColor: '#cfcfcf',
+    borderRadius: 20,
+    color: 'black',
+    fontSize: 16
+  }
+})
+
+export default SearchBarWithAutocomplete
